6e2fad6efcd42c8881fa30fe594def7f170253dc
FavianYumnaF <163508605+FavianYumnaF@users.noreply.github.com>
2024-04-04 14:54:32 +0700

    initial comiit

---
+++ saving_app(Pertemua 6-7)/lib/styles/colors.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+const Color kBgColor = Color(0xFFADC8FF);
+const Color kBlueRibbon = Color(0xFF3366ff);
+const Color kNightBlack = Color(0xFF2C2C2C);
+const Color kBlack = Color(0xFF000000);
+const Color kMatterhornBlack = Color(0xFF525252);
+const Color kNobleGray = Color(0xFF9F9F9F);
+const Color kLightGray = Color(0xFFCBCBCB);
+const Color kSuvaGray = Color(0xFF868686);
+const Color kGrey = Color(0xFF888888);
+const Color kDarkGray = Color(0xFFababab);
+const Color kWhite = Color(0xFFffffff);
+const Color kEgyptianBlue = Color(0xFF102693);
+const Color kLuckyBlue = Color(0xFF363853);
+const Color kTropicalBlue = Color(0xFFADC8FF);
+const Color kTreeGreen = Color(0xFF6ED097);
+const Color kOrange = Color(0xFFF65725);
